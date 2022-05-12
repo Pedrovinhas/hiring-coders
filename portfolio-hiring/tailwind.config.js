8545edaec451683eb032a7f97c652a20c9fd4481
pedrovinhas <pstvideos@gmail.com>
2022-05-12 00:34:04 -0300

    final adjusts

--- portfolio-hiring/tailwind.config.js
+++ portfolio-hiring/tailwind.config.js
@@ -5,16 +5,26 @@ module.exports = {
       fontFamily: {
         'roboto': ['Roboto', 'sans-serif'],
         'roboto-medium': ['Roboto Medium', 'sans-serif'],
-        'roboto-black': ['Roboto Black', 'sans-serif']
+        'roboto-mono': ['Roboto Mono', 'monospace']
       },
       colors: {
         'primary': '#f88f20',
         'paragraph': '#3f3d56',
         'backgroundColor': '#181720',
+        'secondary-soft': 'b5b1b1',
         'secondary': '#f1f1f1'
       },
       screens: {
         'sm': '640px',
+      },
+      animation: {
+        fade: 'fadeIn 5s ease-in-out'
+      },
+      keyframes: {
+        fadeIn: {
+          ' from': { opacity: 0},
+          'to':{ opacity: 1 }
+        }
       }
     },
   },
